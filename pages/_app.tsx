3110ee2d609424bb6f078a05e1c4a0ffca6d4c7f
Ulisseus <newdaddy1911@gmail.com>
2021-07-21 19:48:13 +0300

    change favicon, title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="title" content="Star Wars Ships" />
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default MyApp;
