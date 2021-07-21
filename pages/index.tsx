bb87999dc99c0ae4a191eb7cd4ddd5f9f3a2a307
Ulisseus <newdaddy1911@gmail.com>
2021-07-21 17:12:56 +0300

    refactor index

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,20 +5,33 @@ import ships from "../dummyData/ships";
 import Ship from "../types/ship";
 import styles from "../styles/index.module.scss";
 
+const Nav: React.FC = () => {
+  return (
+    <nav className="bg-secondary text-white px-4 py-3 text-center border-bottom border-primary">
+      <h1 className={`${styles.title} text-primary bg-secondary m-auto`}>
+        Star wars ships
+      </h1>
+      <div className="text-primary">
+        In some far-far away galaxy someone decided to compare all starships...
+      </div>
+    </nav>
+  );
+};
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-secondary text-primary text-center border-top border-primary">
+      <b className="py-1 d-inline-block">2021</b>
+    </footer>
+  );
+};
+
 const App: React.FC = () => {
   const [state, setState] = useState<"selection" | "comparision">("selection");
   const [shipsToCompare, setShipsToCompare] = useState<Ship[]>([]);
   return (
     <div className="d-flex flex-column min-vh-100">
-      <nav className="bg-secondary text-white px-4 py-3 text-center border-bottom border-primary">
-        <h1 className={`${styles.title} text-primary bg-secondary m-auto`}>
-          Star wars ships
-        </h1>
-        <div className="text-primary">
-          In some far-far away galaxy someone decided to compare all
-          starships...
-        </div>
-      </nav>
+      <Nav />
       <main
         className={`${styles.main} d-flex flex-column justify-content-center bg-gray`}
       >
@@ -33,9 +46,7 @@ const App: React.FC = () => {
           <Comparision setState={setState} ships={shipsToCompare} />
         )}
       </main>
-      <footer className="bg-secondary text-primary text-center border-top border-primary">
-        <b className="py-1 d-inline-block">2021</b>
-      </footer>
+      <Footer />
     </div>
   );
 };
