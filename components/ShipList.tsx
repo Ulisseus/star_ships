8c8cec6dad506cdf8d9fe2999e2ff6c14b6840a5
Ulisseus <newdaddy1911@gmail.com>
2021-07-21 17:18:58 +0300

    refactor ShipList

--- components/ShipList.tsx
+++ components/ShipList.tsx
@@ -8,14 +8,15 @@ const ShipList: React.FC<{
   setShipToCompare: React.Dispatch<React.SetStateAction<Ship[]>>;
   shipsToCompare: Ship[];
 }> = ({ ships, setState, setShipToCompare, shipsToCompare }) => {
+  const onClick = (ship: Ship) => {
+    if (shipsToCompare.map((s) => s.name).includes(ship.name)) {
+      setShipToCompare(shipsToCompare.filter((s) => s.name !== ship.name));
+    } else setShipToCompare([...shipsToCompare, ship]);
+  };
   const names = ships.map((ship) => (
     <li
       key={ship.name}
-      onClick={() => {
-        if (shipsToCompare.map((s) => s.name).includes(ship.name)) {
-          setShipToCompare(shipsToCompare.filter((s) => s.name !== ship.name));
-        } else setShipToCompare([...shipsToCompare, ship]);
-      }}
+      onClick={() => onClick(ship)}
       className={
         shipsToCompare.includes(ship)
           ? `card bg-secondary text-primary ${styles.ship} my-1  px-2 py-1`
@@ -25,20 +26,21 @@ const ShipList: React.FC<{
       {ship.name}
     </li>
   ));
+  const button = (
+    <button
+      type="button"
+      className="btn btn-secondary mx-3 my-2 text-primary border border-primary"
+      onClick={() => {
+        setState("comparision");
+      }}
+    >
+      <b className={styles.button}>Compare selected</b>
+    </button>
+  );
   return (
     <>
       <ul className={`${styles.list} mx-2`}>{names}</ul>
-      <div className="d-flex justify-content-center">
-        <button
-          type="button"
-          className="btn btn-secondary mx-3 my-2 text-primary border border-primary"
-          onClick={() => {
-            setState("comparision");
-          }}
-        >
-          <b className={styles.button}>Compare selected</b>
-        </button>
-      </div>
+      <div className="d-flex justify-content-center">{button}</div>
     </>
   );
 };
